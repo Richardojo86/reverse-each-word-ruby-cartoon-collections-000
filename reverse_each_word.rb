88c7fafc9ad8cf7c3c5776f40aea08098beaca89
richardojo04@gmail.com <richardojo04@gmail.com>
2018-05-12 21:09:51 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,12 @@
-sentence = "Hello, there and how are you?"
-
 def reverse_each_word(sentence)
   new_arry = []
   new_sentence = sentence.split 
-  new_sentence.collect{|s| new_arry << s.reverse}
+  new_sentence.each{|s| new_arry << s.reverse}
   new_arry.join(" ")
 end
   
+def reverse_each_word(sentence)
+  splitted_sentence = sentence.split 
+  reversed_sentence_array = splitted_sentence.collect{|s| s.reverse}
+  reversed_sentence_array.join(" ")
+end
